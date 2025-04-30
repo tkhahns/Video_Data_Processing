9b080f37f475e685264194d43fe2e26b46ebcf36
Kenneth Dao <111437863+tkhahns@users.noreply.github.com>
2025-04-30 18:47:30 +0200

    Remove module download option

--- download_videos/__main__.py
+++
@@ -1,9 +0,0 @@
-"""
-Entry point for running the download_videos package as a module.
-Enables execution with: python -m download_videos
-"""
-import sys
-from download_videos.main import main
-
-if __name__ == "__main__":
-    sys.exit(main())
